95a353c61fce5e596e05280158b7795856e4b4c4
jerry <calljerryli@outlook.com>
2022-11-29 21:29:10 +0800

    update recordStatistics calculation code

--- src/components/RecordChart.tsx
+++ src/components/RecordChart.tsx
@@ -1,78 +1,34 @@
 import { Area, AreaChart, Tooltip, XAxis, YAxis } from 'recharts';
 import { useRecoilState } from 'recoil';
 import { atoms } from '../atoms';
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 
 const recordChart = () => {
   const [recordList, setRecordList] = useRecoilState(atoms.recordList)
-  const [recordStatistics, setRecordStatistics] = useState([] as {
-    date: string,
-    workCount: number,
-    readCount: number,
-    lazyCount: number,
-    sleepCount: number,
-  }[])
-
-  const updateRecordStatistics = () => {
-    const newRecordStatistics = [] as {
-      date: string,
-      workCount: number,
-      readCount: number,
-      lazyCount: number,
-      sleepCount: number,
-    }[]
-    for (const record of recordList) {
-      let newRecordStatisticsItem = newRecordStatistics.find(item => item.date === record.date)
-      if (!newRecordStatisticsItem) {
-        newRecordStatisticsItem = {
-          date: record.date,
-          workCount: 0,
-          readCount: 0,
-          lazyCount: 0,
-          sleepCount: 0,
+  const recordStats = useMemo(() => (
+    [...new Set(recordList.map(item => item.date))]
+      .map(date => {
+        const recordListByDate = recordList.filter(recordListItem => recordListItem.date === date)
+        return {
+          date,
+          workCount: recordListByDate.filter(recordListItem => recordListItem.type === '工作').length,
+          readCount: recordListByDate.filter(recordListItem => recordListItem.type === '充电').length,
+          lazyCount: recordListByDate.filter(recordListItem => recordListItem.type === '摸鱼').length,
+          sleepCount: recordListByDate.filter(recordListItem => recordListItem.type === '休息').length,
         }
-        newRecordStatistics.push(newRecordStatisticsItem)
-      }
-      switch (record.type) {
-        case '工作': {
-          newRecordStatisticsItem.workCount += 1
-          break
-        }
-        case '充电': {
-          newRecordStatisticsItem.readCount += 1
-          break
-        }
-        case '摸鱼': {
-          newRecordStatisticsItem.lazyCount += 1
-          break
-        }
-        case '休息': {
-          newRecordStatisticsItem.sleepCount += 1
-          break
-        }
-        default: {
-          break
-        }
-      }
-    }
-    console.log('newRecordStatistics', newRecordStatistics)
-    newRecordStatistics.sort((a, b) => a.date.localeCompare(b.date))
-    setRecordStatistics(newRecordStatistics)
-  }
-
-  useEffect(() => {
-    updateRecordStatistics()
-  }, [recordList])
+      })
+      .sort((a, b) => a.date.localeCompare(b.date))
+  ), [recordList])
 
   return (
     <>
-      {recordStatistics.length > 1 ?
+      {recordStats.length > 1 ?
         <details open>
           <summary>统计图表</summary>
           <AreaChart
             width={800}
             height={200}
-            data={recordStatistics}
+            data={recordStats}
             margin={{
               top: 10,
               right: 30,
